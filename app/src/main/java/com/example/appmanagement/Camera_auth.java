8c53a910763013c30ff016e94ce8ec0c3ba48971
leonTech254 <martinleontech23@gmail.com>
2023-02-22 15:16:44 +0300

    --update

---
+++ app/src/main/java/com/example/appmanagement/Camera_auth.java
@@ -0,0 +1,14 @@
+package com.example.appmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Camera_auth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_auth);
+    }
+}
